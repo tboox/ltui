14fb32a16cc647bfd337053f5a027443245098ec
ruki <waruqi@gmail.com>
2020-11-21 00:36:03 +0800

    remove boxdialog/mouseable

--- src/ltui/boxdialog.lua
+++ src/ltui/boxdialog.lua
@@ -66,7 +66,6 @@ function boxdialog:init(name, bounds, title)
     end)
 
     -- on click for frame
-    self:option_set("mouseable", true)
     self:frame():action_set(action.ac_on_clicked, function (v, x, y)
 
         -- get relative coordinates
